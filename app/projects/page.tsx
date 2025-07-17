f0d24b0a7885911db618e1ee611b1ff4fd6bb26a
Abhinay Jangde <abhinayjangde@gmail.com>
2025-07-17 22:32:56 +0530

    fix padding in projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,9 @@ import RecentWorks from "@/components/RecentWorks";
 export default function Projects(){
 
     return (
+        <div className="p-2">
+
         <RecentWorks/>
+        </div>
     )
 }
