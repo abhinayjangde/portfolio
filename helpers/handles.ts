f6dce508999290d1ad0795b9ab7740cad808017c
Abhinay Jangde <abhinayjangde@gmail.com>
2025-11-02 17:15:53 +0530

    add discord server handle

--- helpers/handles.ts
+++ helpers/handles.ts
@@ -1,8 +1,8 @@
-
 export const handles = {
-    youtube : "https://www.youtube.com/@abhinayjangde",
-    github : "https://www.github.com/abhinayjangde",
-    linkedin : "https://www.linkedin.com/in/abhinay-jangde-a195011b9/",
-    x : "https://x.com/AbhinayJangde",
-    instagram: "https://www.instagram.com/abhinayjangde"
-}
+    youtube: "https://www.youtube.com/@abhinayjangde",
+    github: "https://www.github.com/abhinayjangde",
+    linkedin: "https://www.linkedin.com/in/abhinay-jangde-a195011b9/",
+    x: "https://x.com/AbhinayJangde",
+    instagram: "https://www.instagram.com/abhinayjangde",
+    discord: "https://discord.com/invite/CxPBRSZut7",
+};
