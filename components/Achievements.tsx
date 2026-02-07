f0dccb659fdf2150dad6b31b6fe3695e85b33a03
Abhinay Jangde <abhinayjangde@gmail.com>
2026-02-07 12:31:45 +0530

    feat: add Achievements component displaying YouTube course and related links

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Achievements() {
     return (
@@ -12,15 +13,20 @@ export default function Achievements() {
                 YouTube.
             </p>
             <section className="">
-                <div className="mt-6 text-center p-6 bg-gray-50 dark:bg-neutral-900 rounded-lg border border-gray-200 dark:border-gray-800">
-                    <h3 className="text-xl font-bold mb-2 text-gray-800 dark:text-gray-100">
+                <div className="mt-6 text-center bg-gray-50 dark:bg-neutral-900 rounded-lg">
+                    {/* 
+                    */}
+
+                    <Image src={"/yt.png"} alt="" width={1300} height={386} className="rounded-lg" />
+                    <h3 className="text-xl mt-4 font-bold mb-2 text-gray-800 dark:text-gray-100">
                         Want to learn more?
                     </h3>
                     <p className="text-gray-600 dark:text-gray-400 mb-6">
                         Check out my YouTube channel for more tutorials and my
                         website for structured courses.
                     </p>
-                    <div className="flex flex-wrap justify-center gap-4">
+
+                     <div className="flex flex-wrap justify-center gap-4">
                         <Link
                             href="https://www.youtube.com/@abhinayjangde"
                             target="_blank"
